3fbf6b6eeba0985510843562230b0830726c13f9
«Pavel_Chertykovtsev» <palych@mgorsk.ru>
2022-09-05 10:22:37 +0500

    Решение задачи 25

---
+++ HomeTaskNumber4/Task25/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+
+
+//Метод возведения числа A в натуральную степень B
+
+int AToBPower(int A, int B)
+{
+    int result = A;
+
+    for (int i = 1; i < B; i++)
+    {
+        result = result * A;
+    }
+    return result;
+}
+
+Console.Write("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int res = AToBPower(numberA, numberB);
+Console.WriteLine(res);
