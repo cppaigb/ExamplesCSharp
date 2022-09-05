a3f1fd49f8d04807d29d0fb3e21d6d80c1d25607
«Pavel_Chertykovtsev» <palych@mgorsk.ru>
2022-09-05 11:15:27 +0500

    Решение задачи 27

--- HomeTaskNumber4/Task27/Program.cs
+++ HomeTaskNumber4/Task27/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//Метод поиска количества цифр в числе
+
+int AmountDigitInNumber(int number)
+{
+    int amountDig = 1;
+
+    while (number / 10 != 0)
+    {
+        amountDig++;
+        number = number / 10;
+    }
+
+    return amountDig;
+}
+
+
+//Метод, выдает сумму цифр в числе
+int SumDigits(int number, int amount)
+{
+    int sum = 0;
+    for (int i = 0; i < amount; i++)
+    {
+        sum = sum + (number % 10);
+        number = number / 10;
+    }
+    return sum;
+}
+
+
+Console.Write("Введите число: ");
+
+// Проверяем введено ли число
+
+bool isNumberN= int.TryParse(Console.ReadLine(), out int inputNumber);
+
+if (isNumberN != true)
+{
+    Console.WriteLine("Вы ввели не число");
+    return;
+}
+
+int amount = AmountDigitInNumber(inputNumber);
+Console.WriteLine($"Количество цифр в введенном числе: {amount}");
+int sumNumbers = SumDigits(inputNumber, amount);
+Console.WriteLine($"Cумма цифр в введенном числе: {sumNumbers}");
