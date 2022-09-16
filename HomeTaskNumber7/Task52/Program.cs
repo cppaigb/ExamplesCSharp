650a2ceca89a14f94d91dcca3695433e996c6805
«Pavel_Chertykovtsev» <palych@mgorsk.ru>
2022-09-16 21:48:12 +0500

    Решение задачи 52

--- HomeTaskNumber7/Task52/Program.cs
+++ HomeTaskNumber7/Task52/Program.cs
@@ -1,67 +1,135 @@
-﻿/* Задайте двумерный массив из целых чисел. 
-   Найдите среднее арифметическое элементов в каждом столбце. 
-*/
-Console.WriteLine("Введите количество строк");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите количество столбцов");
-int n = Convert.ToInt32(Console.ReadLine());
+﻿// /* Задайте двумерный массив из целых чисел. 
+//    Найдите среднее арифметическое элементов в каждом столбце. 
+// */
 
-int[,] mas = new int[m, n];
+// void PrintMatrix(int[,] matr)
+// {
 
-void PrintArray(int[,] ma)
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             Console.Write($"{matr[i, j]} ");
+//         }
+//         Console.WriteLine("");
+//     }
+// }
+
+// //метод создает двумерный массив из случайных чисел от 1 до 9.
+// double[,] FillArray(int m, int n)
+// {
+// double[,] array = new double[m, n];
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+//     return array;
+// }
+// // метод ищет среднее арифметическое каждого столбца
+// double[] AverageNumbersOfEveryColumn(double[,] array)
+// {
+// double[] avgNumbers = new double[array.GetLength(1)];
+
+// for (int i = 0; i < array.GetLength(1); i++)
+// {
+//     double result = 0.0;
+//     for (int j = 0; j < array.GetLength(0); j++)
+//     {
+//         result += array[j, i];
+//     }
+//     avgNumbers[i] = result / array.GetLength(0);
+// }
+// return avgNumbers;
+// }
+
+// // Функция вывода массива в терминал 
+// void PrintArray(double[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i]);
+//         if (i == array.Length -1) Console.Write(".");
+//         else Console.Write("; ");
+//     }
+//    }
+
+
+// Console.Write("Введите количество столбцов m: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество строк n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// double[,] newArray = FillArray(n, m);
+// FillArray(newArray);
+// PrintMatrix(newArray);
+
+// double[] resaultAverage = AverageNumbersOfEveryColumn(newArray);
+// PrintArray(resaultAverage);
+
+// int rows = 3;
+// int colums = 4;
+// int[,] numbers = new int[rows, colums];
+// FillArray2D(numbers);
+// PrintArray2D(numbers);
+
+
+// Заполнение массива рандомными вещественными числами
+void FillArray2D(int[,] array)
 {
-    for (int i = 0; i < mas.GetLength(0); i++)
-    {
-        for (int j = 0; j < mas.GetLength(1); j++)
-        {
-            Console.Write($"{ma[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-}
-int[,] FillArray(int[,] ma)
-{
-    int[,] mass = ma;
-    Random rnd = new Random();
-    for (int i = 0; i < mas.GetLength(0); i++)
-    {
-        for (int j = 0; j < mas.GetLength(1); j++)
-        {
-            mass[i, j] = rnd.Next(1, 30);
-        }
-    }
-    return mass;
-
-}
-
-FillArray(mas);
-PrintArray(mas);
-
-Console.WriteLine("Введите число из массива:"); 
-int guessNumber = Convert.ToInt32(Console.ReadLine());
-string GuessArrayNumber(int[,] array,int number)
-{
-string some = "";
-    string someString = "";
-    
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            
-            if (number == array[i, j])
-            {  
-                someString = some;
-                some= $"{i}, {j}";
-            }
-            else{
-                Console.WriteLine("Такого числа нет");
-            }
-           
-        }          
-        
+            array[i, j] = new Random().Next(1, 10);
+        }
     }
-    return someString;
 }
 
-Console.WriteLine(GuessArrayNumber(mas, guessNumber));
+//  Функция вывода двумерного массива в терминал
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+// Функция вывода массива в терминал 
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите количество столбцов m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество строк n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[m, n];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+double[] avgNumbers = new double[numbers.GetLength(1)];
+
+for (int i = 0; i < numbers.GetLength(1); i++)
+{
+    double result = 0.0;
+    for (int j = 0; j < numbers.GetLength(0); j++)
+    {
+        result += numbers[j, i];
+    }
+    avgNumbers[i] = result / numbers.GetLength(0);
+}
+PrintArray(avgNumbers);
+
