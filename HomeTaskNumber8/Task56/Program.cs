41c09975d9b417d22c509181dbbb497465c836d5
«Pavel_Chertykovtsev» <palych@mgorsk.ru>
2022-09-19 00:18:18 +0500

    Решение задачи 56

---
+++ HomeTaskNumber8/Task56/Program.cs
@@ -0,0 +1,63 @@
+﻿/* Задайте прямоугольный двумерный массив. Напишите программу, 
+которая будет находить строку с наименьшей суммой элементов. */
+
+
+// Функция вывода номера строки (не индекса) с наименьшей суммой элементов 
+void NumberRowWithMinSum(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка с наименьшей суммой элементов");
+}
+
+//метод выводит матрицу на экран
+void PrintMatrix(int[,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine("");
+    }
+}
+
+//метод создает двумерный массив из случайных чисел от 1 до 9.
+void FillArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+Console.Write("Введите количество столбцов m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество строк n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] newArray = new int[m, n];
+FillArray(newArray);
+PrintMatrix(newArray);
+Console.WriteLine();
+NumberRowWithMinSum(newArray);
