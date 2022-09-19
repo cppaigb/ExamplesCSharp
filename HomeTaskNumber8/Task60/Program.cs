0348cc4e49197b0710e89eaa772ea886049807bd
«Pavel_Chertykovtsev» <palych@mgorsk.ru>
2022-09-19 09:05:17 +0500

    Решение задачи 62

--- HomeTaskNumber8/Task60/Program.cs
+++ HomeTaskNumber8/Task60/Program.cs
@@ -3,22 +3,22 @@
 Массив размером 2 x 2 x 2
  */
 
-int[,,] array3D = new int[2, 2, 2];
-FillArray(array3D);
-PrintIndex(array3D);
+// int[,,] array3D = new int[2, 2, 2];
+// FillArray(array3D);
+// PrintIndex(array3D);
 
 
 // Метод вывода индекса элементов 3D массива
 void PrintIndex(int[,,] array)
 {
-    for (int i = 0; i < array3D.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < array3D.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
             Console.WriteLine();
-            for (int k = 0; k < array3D.GetLength(2); k++)
+            for (int k = 0; k < array.GetLength(2); k++)
             {
-                Console.Write($"{array3D[i, j, k]}({i},{j},{k}) ");
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
             }
         }
     }
@@ -40,3 +40,13 @@ void FillArray(int[,,] array)
         }
     }
 }
+
+Console.Write("Введите количество столбцов m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество строк n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите i: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int[,,] newArray = new int[m, n, i];
+FillArray(newArray);
+PrintIndex(newArray);
