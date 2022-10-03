3d09a5c893dd6bacf69cb783eb117e1608391e48
«Pavel_Chertykovtsev» <palych@mgorsk.ru>
2022-10-03 09:16:39 +0500

    Решение задачи 64

--- HomeTaskNumber9/Task64/Program.cs
+++ HomeTaskNumber9/Task64/Program.cs
@@ -1,1 +1,27 @@
 ﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Натуральные числа в промежутке от M до N равна: ");
+NumbersFromMToN(m, n);
+
+// функция поиска натуральных чисел от M до N
+void NumbersFromMToN(int m, int n)
+{
+
+    if (m > n)
+    {
+        return;
+    }
+    else
+    {
+
+        NumbersFromMToN(m, n - 1);
+        Console.Write($"{n}, ");
+    }
+}
+
+
